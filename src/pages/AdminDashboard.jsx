1ac2ae84768264840c101acaac54dbda7357c612
astridayala <astriayala06@gmail.com>
2025-03-25 10:41:01 -0600

    formato

--- src/pages/AdminDashboard.jsx
+++ src/pages/AdminDashboard.jsx
@@ -70,28 +70,27 @@ function AdminDashboard() {
 
         {/* Cupones Pendientes */}
         <h2 className="text-xl font-bold mb-4 text-center">Cupones Pendientes</h2>
-        <div>
+        <div className="cupones grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-2 p-1 rounded-lg place-items-center">
           {cupones && cupones.length > 0 ? (
             cupones.map(cupon => cupon && (
-              <div key={cupon.id} className="mb-6 p-4 border rounded shadow">
-                <h3 className="font-semibold">{cupon.titulo ? cupon.titulo : "Sin título"}</h3>
-                <p><strong>Detalle:</strong> {cupon.detalles ? cupon.detalles : "No disponible"}</p>
-                <p><strong>Precio Oferta:</strong> ${cupon.precioOferta ? cupon.precioOferta : "N/A"}</p>
-                <p><strong>Precio Regular:</strong> ${cupon.precioRegular ? cupon.precioRegular : "N/A"}</p>
-                <p><strong>Fecha Inicio:</strong> {formatFecha(cupon.fechaInicio)}</p>
-                <p><strong>Fecha Fin:</strong> {formatFecha(cupon.fechaFin)}</p>
-
-                {cupon.precioRegular && cupon.precioOferta && (
+              <div key={cupon.id} className="bg-[#c0c8cf] rounded-lg shadow p-4 text-center mb-7 w-72 h-110 flex flex-col items-center justify-center">
+                <h3 className="monse text-lg font-extrabold text-[#1d3557] mb-2 uppercase">{cupon.titulo ? cupon.titulo : "Sin título"}</h3>
+                {cupon.imagenURL && <img src={cupon.imagenURL} alt="Cupón" className="w-auto h-35 mb-3 mx-auto block" />}
+                <p className="mb-2 text-gray-700">{cupon.detalles ? cupon.detalles : "No disponible"}</p>
+                <div className="text-justify px-4">
+                  <p><strong>Precio Oferta:</strong> ${cupon.precioOferta ? cupon.precioOferta : "N/A"}</p>
+                  <p><strong>Precio Regular:</strong> ${cupon.precioRegular ? cupon.precioRegular : "N/A"}</p>
+                  <p><strong>Fecha Inicio:</strong> {formatFecha(cupon.fechaInicio)}</p>
+                  <p><strong>Fecha Fin:</strong> {formatFecha(cupon.fechaFin)}</p>
+                 {cupon.precioRegular && cupon.precioOferta && (
                   <p>
                     <strong>Descuento:</strong> {((1 - cupon.precioOferta / cupon.precioRegular) * 100).toFixed(2)}%
                   </p>
                 )}
-
-                {cupon.imagenURL && <img src={cupon.imagenURL} alt="Cupón" className="w-40 h-40 object-cover mt-2 rounded" />}
-
+                </div>
                 <div className="space-x-2 mt-2">
-                  <button onClick={() => aprobarCupon(cupon.id)} className="bg-green-500 text-white px-3 py-1 rounded">Aprobar</button>
-                  <button onClick={() => rechazarCupon(cupon.id)} className="bg-red-500 text-white px-3 py-1 rounded">Rechazar</button>
+                <button onClick={() => rechazarCupon(cupon.id)} className="bg-[#ff2323] text-white font-bold py-2 px-4 rounded-lg hover:bg-[#ff5757] transition hover:scale-103">Rechazar</button>
+                  <button onClick={() => aprobarCupon(cupon.id)} className="bg-[#3C7499] text-white font-bold py-2 px-4 rounded-lg hover:bg-[#6da3c3] transition hover:scale-103">Aprobar</button>
                 </div>
               </div>
             ))
